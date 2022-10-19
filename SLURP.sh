adf80fb9495c1876e30a4ed88cd3d6f65d2a4d72
Amoldeep Kainth <66103719+kainth-amoldeep@users.noreply.github.com>
2022-10-19 15:31:37 -0500

    Update SLURP.sh

--- SLURP.sh
+++ SLURP.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
-temp="$1" # input filename
+#input primer sequences
+primer1=GCTCTATCTTCTTT
+primer2=CTGATATTGCTGGG
+rc_primer2=CCCAGCAATATCAG
+temp="$1"
 basename=${temp%.*}
 # calculate number of reads in the parent file
 parent_reads=$(wc -l ${temp} | awk '{print $1 * 0.25}')
@@ -7,18 +11,20 @@ echo $parent_reads "reads found in" $temp
 # extract reads that are minimum 100 nt in length
 seqkit seq -g -m 100 ${temp} > "$basename"_minimum100bp.fq 
 # extract reads with q1 sequence in the first 100 bp of positive strand and allowing 2 mismatches 
-cat "$basename"_minimum100bp.fq | seqkit grep -s -i -P -R 1:100 -m 2 -p GCTCTATCTTCTTT -o "$basename"_q1m2.fq
+cat "$basename"_minimum100bp.fq | seqkit grep -s -i -P -R 1:100 -m 2 -p $primer1 -o "$basename"_q1m2.fq
 wc -l "$basename"_q1m2.fq | awk '{print $1 * 0.25, "reads found in '$basename'_q1m2.fq", $1*0.25/'$parent_reads'*100, "% of parent"}'
 # extract reads with q3 sequence in the first 100 bp of positive strand and allowing 2 mismatches 
-cat "$basename"_minimum100bp.fq | seqkit grep -s -i -P -R 1:100 -m 2 -p CTGATATTGCTGGG -o "$basename"_q3m2.fq
+cat "$basename"_minimum100bp.fq | seqkit grep -s -i -P -R 1:100 -m 2 -p $primer2 -o "$basename"_q3m2.fq
 wc -l "$basename"_q3m2.fq | awk '{print $1 * 0.25, "reads found in '$basename'_q3m2.fq", $1*0.25/'$parent_reads'*100, "% of parent"}'
 # extract reads with q3rc sequence in the last 100 bp of positive strand and allowing 2 mismatches 
-cat "$basename"_minimum100bp.fq | seqkit grep -s -i -P -R -100:-1 -m 2 -p CCCAGCAATATCAG -o "$basename"_q3m2rc.fq
+cat "$basename"_minimum100bp.fq | seqkit grep -s -i -P -R -100:-1 -m 2 -p $rc_primer2 -o "$basename"_q3m2rc.fq
 wc -l "$basename"_q3m2rc.fq | awk '{print $1 * 0.25, "reads found in '$basename'_q3m2rc.fq", $1*0.25/'$parent_reads'*100, "% of parent"}'
 # making reverse complement of q3m2
 seqkit seq  -t dna -r -p "$basename"_q3m2.fq > "$basename"_rcq3m2.fq
 # combining q1m2 and q3m2rc and removing duplicates
 cat "$basename"_q1m2.fq "$basename"_q3m2rc.fq | seqkit rmdup -o "$basename"_q1m2_q3m2rc.fq
 # combining q1m2_q3m2rc and rcq3m2 and removing duplicates
-cat "$basename"_q1m2_q3m2rc.fq "$basename"_rcq3m2.fq | seqkit rmdup -o "$basename"_SLURP_stranded.fq
-wc -l "$basename"_SLURP_stranded.fq | awk '{print $1 * 0.25, "reads found in '$basename'_SLURP_stranded.fq", $1*0.25/'$parent_reads'*100, "% of parent"}'
+cat "$basename"_q1m2_q3m2rc.fq "$basename"_rcq3m2.fq | seqkit rmdup -o "$basename"_SLURP_stranded.fastq
+wc -l "$basename"_SLURP_stranded.fastq | awk '{print $1 * 0.25, "reads found in '$basename'_SLURP_stranded.fq", $1*0.25/'$parent_reads'*100, "% of parent"}'
+rm "$basename"_minimum100bp.fq "$basename"_q1m2.fq "$basename"_q3m2.fq "$basename"_q3m2rc.fq "$basename"_rcq3m2.fq "$basename"_q1m2_q3m2rc.fq
+echo "SLURP stranding completed!"
