d124cabcd64ac8b265dabb09144cfa2d4bd539ef
Amoldeep Kainth <66103719+kainth-amoldeep@users.noreply.github.com>
2022-10-21 11:30:14 -0500

    Update SLURP.sh

--- SLURP.sh
+++ SLURP.sh
@@ -3,6 +3,7 @@
 primer1=GCTCTATCTTCTTT
 primer2=CTGATATTGCTGGG
 rc_primer2=CCCAGCAATATCAG
+mismatch=2
 temp="$1"
 basename=${temp%.*}
 # calculate number of reads in the parent file
@@ -11,13 +12,13 @@ echo $parent_reads "reads found in" $temp
 # extract reads that are minimum 100 nt in length
 seqkit seq -g -m 100 ${temp} > "$basename"_minimum100bp.fq 
 # extract reads with q1 sequence in the first 100 bp of positive strand and allowing 2 mismatches 
-cat "$basename"_minimum100bp.fq | seqkit grep -s -i -P -R 1:100 -m 2 -p $primer1 -o "$basename"_q1m2.fq
+cat "$basename"_minimum100bp.fq | seqkit grep -s -i -P -R 1:100 -m $mismatch -p $primer1 -o "$basename"_q1m2.fq
 wc -l "$basename"_q1m2.fq | awk '{print $1 * 0.25, "reads found in '$basename'_q1m2.fq", $1*0.25/'$parent_reads'*100, "% of parent"}'
 # extract reads with q3 sequence in the first 100 bp of positive strand and allowing 2 mismatches 
-cat "$basename"_minimum100bp.fq | seqkit grep -s -i -P -R 1:100 -m 2 -p $primer2 -o "$basename"_q3m2.fq
+cat "$basename"_minimum100bp.fq | seqkit grep -s -i -P -R 1:100 -m $mismatch -p $primer2 -o "$basename"_q3m2.fq
 wc -l "$basename"_q3m2.fq | awk '{print $1 * 0.25, "reads found in '$basename'_q3m2.fq", $1*0.25/'$parent_reads'*100, "% of parent"}'
 # extract reads with q3rc sequence in the last 100 bp of positive strand and allowing 2 mismatches 
-cat "$basename"_minimum100bp.fq | seqkit grep -s -i -P -R -100:-1 -m 2 -p $rc_primer2 -o "$basename"_q3m2rc.fq
+cat "$basename"_minimum100bp.fq | seqkit grep -s -i -P -R -100:-1 -m $mismatch -p $rc_primer2 -o "$basename"_q3m2rc.fq
 wc -l "$basename"_q3m2rc.fq | awk '{print $1 * 0.25, "reads found in '$basename'_q3m2rc.fq", $1*0.25/'$parent_reads'*100, "% of parent"}'
 # making reverse complement of q3m2
 seqkit seq  -t dna -r -p "$basename"_q3m2.fq > "$basename"_rcq3m2.fq
